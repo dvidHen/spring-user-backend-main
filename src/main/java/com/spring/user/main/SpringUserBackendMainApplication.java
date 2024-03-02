9b877d305a82df6f00dea09d73b8912cccc08087
Henrry Ramirez <henrry.ramirez@hegga.cl>
2024-03-01 22:17:21 -0300

    Carga de ambiente

---
+++ src/main/java/com/spring/user/main/SpringUserBackendMainApplication.java
@@ -0,0 +1,13 @@
+package com.spring.user.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserBackendMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserBackendMainApplication.class, args);
+	}
+
+}
